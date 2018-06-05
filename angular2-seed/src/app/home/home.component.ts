6e099c41d9f155382a2e6765b89167865dff6443
Josh Tate <Josh.Tate@hobbylobby.com>
2018-06-05 14:38:11 -0500

    Backup push

---
+++ angular2-seed/src/app/home/home.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { Employee } from '../models/employee.model';
+import { FormPoster } from '../services/form-poster.service';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'home',
+  styleUrls: ['./home.component.css'],
+  templateUrl: './home.component.html'
+})
+
+export class HomeComponent {
+  languages = [];
+  model = new Employee('', '', true, 'w2','default');
+  hasPrimaryLanguageError = false;
+
+  constructor(private formPoster: FormPoster){
+    this.formPoster.getLanguages()
+          .subscribe(
+            data => this.languages = data.languages,
+            err => console.log('get error: ', err)
+          );
+  }
+
+  submitForm(form: NgForm){
+    // validate form
+    this.validatePrimaryLanguage(this.model.primaryLanguage);
+    if(this.hasPrimaryLanguageError){
+      return;
+    }
+
+    this.formPoster.postEmployeeForm(this.model).subscribe(
+        data => console.log('success: ', data),
+        err => console.log('error: ', err)
+      )
+  }
+
+  validatePrimaryLanguage(value){
+    if(value === 'default'){
+      this.hasPrimaryLanguageError = true;
+    } else {
+      this.hasPrimaryLanguageError = false;
+    }
+  }
+}
